ebe7eb495ab1d9c10de9053ee76bb44a5b306a30
JFH000 <jf.hernandezp1@uniandes.edu.co>
2025-04-23 12:06:28 -0500

    try env

--- map_no_django/gcs_settings.py
+++ map_no_django/gcs_settings.py
@@ -3,5 +3,7 @@ import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ""
 
+print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+
 GCS_BUCKET_NAME = "examenes-eeg"
 GCS_BASE_FOLDER = "uploads/"
